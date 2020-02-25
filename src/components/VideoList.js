4d42520941c904b0b0b8ecad7b26600754f105cd
ustrojny <ustrojny@gmail.com>
2020-02-25 15:43:34 +0100

    fixed typo

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,7 @@ import VideoItem from './VideoItem';
 
 const VideoList = ({ videos, onVideoSelect }) => { // destrukturyzacja zamiast (props)
     const renderedList = videos.map((video) => {
-        return <VideoItem onVideSelect = { onVideoSelect } video = { video } />;
+        return <VideoItem onVideoSelect = { onVideoSelect } video = { video } />;
     });
 
     return <div className="ui relaxed divided list">{renderedList}</div> 
